13bb1775f098d157aff959f585cce100fe0f433e
Luden <mdargie97@gmail.com>
2025-11-24 18:20:24 +0000

    working openai ask request

--- library/commands/openai/openai-ask.js
+++ library/commands/openai/openai-ask.js
@@ -1,19 +1,34 @@
 import { SlashCommandBuilder, AttachmentBuilder } from "discord.js";
 import config from "../../../config/config.json" with { type: "json" };
-
+import { AskBasic } from "../../modules/openai/openai-helper.js";
 
 export default {
     cooldown: 10,
     data: new SlashCommandBuilder()
     .setName('ask')
-    .setDescription('Ask ChiralBot...'),
+    .setDescription('Ask ChiralBot...')
+    .addStringOption((option) =>
+      option
+        .setName("question")
+        .setDescription("Question input to ask ChiralBot")
+        .setRequired(true)
+    ),
     async execute(interaction){
         try{
-            
+            if(interaction && interaction.options.getString("question")){
+                await interaction.reply({ content: `thinking...` });
+                const normalisedMessage = interaction.options.getString("question").replace(/[^a-zA-Z0-9 ]/g,"");
+                const openAiResponse = await AskBasic(normalisedMessage);
+
+                if(openAiResponse){
+                    await interaction.followUp({
+                        content: `${openAiResponse}`,
+                    });
+                }
+            }
         }
         catch(e){
             console.log('[ERROR] Error processing user command - [ask]', e)
         }
-
     }
 }
--- library/modules/openai/openai-helper.js
+++ library/modules/openai/openai-helper.js
@@ -1,15 +1,31 @@
 import OpenAI from "openai";
 
-// Load environment variables first
-//dotenv.config();
-
 const client = new OpenAI({
-    apiKey: process.env.OPENAI_API_KEY,
+  apiKey: process.env.OPENAI_API_KEY,
 });
 
-const response = await client.responses.create({
-    model: "gpt-5",
-    input: "Write a one-sentence bedtime story about a unicorn."
-});
+const selecteModel = "gpt-5";
 
-console.log(response.output_text);
+export const AskBasic = async (userInputString) => {
+  try {
+    console.log(
+      "[OPENAI-CREATE] starting OpenAI request for [AskBasic] with input :",
+      userInputString
+    );
+    const response = await client.responses.create({
+      model: selecteModel,
+    //   tools: [{ type: "web_search" }],
+      input: userInputString,
+    });
+    console.log(
+      "[OPENAI-RESPONSE] ",
+      `${response.status} - ${response.model} - ${response.output_text}`
+    );
+    if (response.status === "completed") return response.output_text;
+  } catch (e) {
+    console.log(
+      "[OPENAI-ERROR] error processing OpenAI request for [AskBasic]",
+      e
+    );
+  }
+};
